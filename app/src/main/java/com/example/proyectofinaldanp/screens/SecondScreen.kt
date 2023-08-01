f6113e9841d729134e81cd3875922578a37055db
EmersonQH <64039337+EmersonQH@users.noreply.github.com>
2023-07-31 22:51:06 -0500

    Agregando la opcion de subir imagen en el SecondScreen

--- app/src/main/java/com/example/proyectofinaldanp/screens/SecondScreen.kt
+++ app/src/main/java/com/example/proyectofinaldanp/screens/SecondScreen.kt
@@ -1,17 +1,22 @@
 package com.example.proyectofinaldanp.screens
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -53,6 +58,45 @@ fun SecondBodyContent(navController: NavController){
         )
         Spacer(modifier = Modifier.height(10.dp))
         Text(text = "Ingrese una imagen para identificar su planta")
+
+
+        var imageUri by remember { mutableStateOf<Uri?>(null) }
+        val context = LocalContext.current
+        val bitmap = remember { mutableStateOf<Bitmap?>(null) }
+
+        val launcher =
+            rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+                imageUri = uri
+            }
+        imageUri?.let {
+            if (Build.VERSION.SDK_INT < 28) {
+                bitmap.value = MediaStore.Images
+                    .Media.getBitmap(context.contentResolver, it)
+            } else {
+                val source = ImageDecoder.createSource(context.contentResolver, it)
+                bitmap.value = ImageDecoder.decodeBitmap(source)
+            }
+
+            bitmap.value?.let { btm ->
+                Image(
+                    bitmap = btm.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(400.dp)
+                        .padding(20.dp)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Gray
+            ),
+            onClick = { launcher.launch("image/*") }) {
+            Text(text = "Pick Image",
+            color = Color.White)
+        }
 
         Spacer(modifier = Modifier.height(180.dp))
 
